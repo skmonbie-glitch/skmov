8922d09a9f738df852c47115b98df591087a1089
john michael <deudajm@gmail.com>
2025-12-10 12:01:51 +0800

    new updates

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQP962eaqxmlxt6J-QicegjPH08DHFE3c",
-  authDomain: "skmovie-46af4.firebaseapp.com",
-  projectId: "skmovie-46af4",
-  storageBucket: "skmovie-46af4.firebasestorage.app",
-  messagingSenderId: "167072317620",
-  appId: "1:167072317620:web:643d60d5a501b3c74297e1",
-  measurementId: "G-YESBQRSPDG",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 
